cc6de84a713d13ce92694f18bc7bb09fa0e0e383
Benjamin Lannon <brlannon118@gmail.com>
2015-12-05 15:16:17 -0500

    Finished day 3

--- houses.rb
+++ houses.rb
@@ -1,26 +1,45 @@
 arr = Array.new(4000) { Array.new(4000, 0) }
 
+rposx = 300
+rposy = 300
+
 posx = 300
 posy = 300
 
 arr[posx][posy] += 1
+arr[rposx][rposy] += 1
+
+ticker = [0,1].cycle
 
 File.open("houses.dat", "r") do |f|
   inner = f.read.split("")
 
   inner.each do |i|
-    case i
-    when '^'
-      posy -= 1
-    when 'v'
-      posy += 1
-    when '<'
-      posx -= 1
-    when '>'
-      posx += 1
+    if ticker.next == 0
+      case i
+      when '^'
+        posy -= 1
+      when 'v'
+        posy += 1
+      when '<'
+        posx -= 1
+      when '>'
+        posx += 1
+      end
+      arr[posx][posy] += 1
+    else
+      case i
+      when '^'
+        rposy -= 1
+      when 'v'
+        rposy += 1
+      when '<'
+        rposx -= 1
+      when '>'
+        rposx += 1
+      end
+      arr[rposx][rposy] += 1
     end
-
-    arr[posx][posy] += 1
   end
 end
 
