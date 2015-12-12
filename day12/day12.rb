67f1edc9c486067c590f4c550362fb5a9b5cf196
Benjamin Lannon <brlannon118@gmail.com>
2015-12-12 11:01:16 -0500

    Rest of day 12

--- day12/day12.rb
+++ day12/day12.rb
@@ -1,1 +1,23 @@
+# Day 12 of Advent of Code
+require 'json'
+
+# pt 1
 File.open('json.dat', 'r') { |f| puts f.readline.scan(/-?[0-9]+/).map{|str| str.to_i }.inject(0, &:+) }
+
+# pt 2
+def parse(input)
+  sum = 0
+  case input
+  when Hash
+    return 0 if input.values.include? "red"
+    input.keys.each { |key| sum += input[key].is_a?(Fixnum) ? input[key] : parse(input[key]) }
+  when Array
+    input.each { |val| sum += val.is_a?(Fixnum) ? val : parse(val) }
+  when Fixnum
+    sum += input
+  end
+  return sum
+end
+
+input = JSON.load(File.read('json.dat'))
+puts parse(input)
